c33bd9eda87cd0c36c66d186727d3540c4b36699
Susovan Ghosh <susovan.ghosh0@gmail.com>
2023-05-17 21:37:32 +0530

    Attemp 2

--- demoLogger/src/main/java/com/exlservice/demoLogger/Layout/CustomJsonLayout.java
+++ demoLogger/src/main/java/com/exlservice/demoLogger/Layout/CustomJsonLayout.java
@@ -1,14 +1,11 @@
 package com.exlservice.demoLogger.Layout;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.TimeZone;
+import java.util.*;
 
 import ch.qos.logback.contrib.json.classic.JsonLayout;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
@@ -25,6 +22,7 @@ public class CustomJsonLayout extends JsonLayout {
     public static final String ENVIRONMENT = "Dev";
     public static final String HEADERS = "http://webservice.careradius.landacorp.com";
     private static final Logger logger = LoggerFactory.getLogger(CustomJsonLayout.class);
+
     public String methodName = "";
 
     @Pointcut(value = "execution(* com.exlservice.demoLogger.Controller.*.*(..) )")
@@ -33,21 +31,39 @@ public class CustomJsonLayout extends JsonLayout {
 
     @Around("myPointcut()")
     public Object logMethodCall(ProceedingJoinPoint pjr) throws Throwable {
-        methodName = pjr.getSignature().getName();
-        System.out.println("Line 36 : " + methodName);
+        Signature signature = pjr.getSignature();
+        Logger logger = LoggerFactory.getLogger(signature.getDeclaringType());
+
+        // These are the information that can be extracted from both ILoggingEvent and ProceedingJoinPoint objects (Although Logger message isn't
+        // available in ProceedingJoinPoint object which forces us to override the method written down below).
+        String loggerName = pjr.getTarget().getClass().getName();
+        methodName = pjr.getSignature().getName(); // This can't be retrieved from ILoggingEvent (main point of concern).
+        String loggerLevel = "";
+        if (logger.isTraceEnabled()) {
+            loggerLevel = "TRACE";
+        } else if (logger.isDebugEnabled()) {
+            loggerLevel = "DEBUG";
+        } else if (logger.isInfoEnabled()) {
+            loggerLevel = "INFO";
+        } else if (logger.isWarnEnabled()) {
+            loggerLevel = "WARN";
+        } else if (logger.isErrorEnabled()) {
+            loggerLevel = "ERROR";
+        }
+
         Object result = pjr.proceed();
         return result;
     }
 
     @Override
     protected void addCustomDataToJsonMap(Map<String, Object> map, ILoggingEvent event) {
-        System.out.println("Line 44 : " + methodName);
         SimpleDateFormat format = new SimpleDateFormat(
                 "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
         format.setTimeZone(TimeZone.getTimeZone("UTC"));
         String timestamp = format.format(new Date());
         Map<String, String> identifiers = new HashMap<String, String>();
         Map<String, String> feature = new HashMap<String, String>();
+
         map.put("Starttime", timestamp);
         identifiers.put("organizationName", APPLICATION);
         identifiers.put("logger", event.getLoggerName());
@@ -60,7 +76,7 @@ public class CustomJsonLayout extends JsonLayout {
         map.put("time", timestamp);
         map.put("identifiers", identifiers);
         map.put("feature", feature);
-        map.put("methodName", methodName);
+        map.put("methodName", event.getCallerData()[0].getMethodName()); // This gives the internal methods but not the actual api method (main point of concern).
         map.put("componentType", "");
         map.put("url", "");
         map.put("httpMethod", HEADERS);
@@ -71,5 +87,4 @@ public class CustomJsonLayout extends JsonLayout {
         map.putAll(event.getMDCPropertyMap());
         super.addCustomDataToJsonMap(map, event);
     }
-
 }
